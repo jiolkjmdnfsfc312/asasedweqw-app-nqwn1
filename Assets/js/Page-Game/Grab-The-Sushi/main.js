8089d3ddeeaee0c9d630cfba803bcb226ed001c6
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-04-18 20:28:32 +0700

    first commit

--- Assets/js/Page-Game/Grab-The-Sushi/main.js
+++ Assets/js/Page-Game/Grab-The-Sushi/main.js
@@ -4,13 +4,13 @@ function getParentUrl() {
             ? document.referrer
             : document.location.href
         : document.location.href;
-    if (document.referrer.indexOf('https://www.fungames.my.id/') !== -1) {
+    if (document.referrer.indexOf('https://jiolkjmdnfsfc312.github.io/asasedweqw-app-nqwn1/') !== -1) {
         url = 'https://gamedistribution.com/';
     }
     return url;
 }
 // Add the self-hosted game url as value of gameUrl.
-var gameUrl = 'https://html5.gamedistribution.com/571357d055374f078e471057d4fcdbea/';
+var gameUrl = 'https://html5.gamedistribution.com/a99a15f6315943129a28cdda9f2962d1/?gd_sdk_referrer_url=https://gamedistribution.com/games/grab-the-sushi';
 document
     .getElementById('FunGames')
     .src = gameUrl + '?gd_sdk_referrer_url=' + getParentUrl();
