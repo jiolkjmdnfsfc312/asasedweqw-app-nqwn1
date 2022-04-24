0779d14272d4ea7fab5feedac311e232a82c2bca
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-04-24 21:09:29 +0700

    first commit

--- Assets/js/Page-Game/Squad-Tower/main.js
+++ Assets/js/Page-Game/Squad-Tower/main.js
@@ -14,3 +14,53 @@ var gameUrl = 'https://html5.gamedistribution.com/3785752826134e79888aa3f724e501
 document
     .getElementById('FunGames')
     .src = gameUrl + '?gd_sdk_referrer_url=' + getParentUrl();
+
+
+// Sidebar Fullscreen
+    var container = document.querySelector('.container');
+    var main = document.querySelector('.main');
+    
+    function toggleSidebar(){
+        isShowingSidebar() ? hideSidebar() : showSidebar();
+    }
+    
+    function showSidebar(){
+        container.classList.add('show-sidebar');
+    }
+    
+    function hideSidebar(){
+        container.classList.remove('show-sidebar');
+    }
+    
+    function isShowingSidebar(){
+        return container.classList.contains('show-sidebar');
+    }
+    
+    document.querySelector('.hamburger').addEventListener('click', toggleSidebar, false);
+    
+    container.addEventListener('click', function(e){
+        if(isShowingSidebar() && main.contains(e.target)){
+            e.preventDefault();
+            hideSidebar();
+        }
+    }, true);
+    
+    // FullScreen
+    function goFullscreen(id) {
+            // Get the element that we want to take into fullscreen mode
+            var element = document.getElementById(id);
+    
+            // These function will not exist in the browsers that don't support fullscreen mode yet,
+            // so we'll have to check to see if they're available before calling them.
+    
+            if (element.mozRequestFullScreen) {
+                // This is how to go into fullscren mode in Firefox
+                // Note the "moz" prefix, which is short for Mozilla.
+                element.mozRequestFullScreen();
+            } else if (element.webkitRequestFullScreen) {
+                // This is how to go into fullscreen mode in Chrome and Safari
+                // Both of those browsers are based on the Webkit project, hence the same prefix.
+                element.webkitRequestFullScreen();
+            }
+            // Hooray, now we're in fullscreen mode!
+        }
