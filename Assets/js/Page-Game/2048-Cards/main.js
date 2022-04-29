f16c045ccbf8d1c52369c9ed0b14a7b658c78c3c
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-04-30 01:33:21 +0700

    first commit

--- Assets/js/Page-Game/2048-Cards/main.js
+++ Assets/js/Page-Game/2048-Cards/main.js
@@ -5,12 +5,12 @@ function getParentUrl() {
             : document.location.href
         : document.location.href;
     if (document.referrer.indexOf('https://jiolkjmdnfsfc312.github.io/asasedweqw-app-nqwn1/') !== -1) {
-        url = 'https://play.gamemonetize.co/';
+        url = 'https://gamedistribution.com/';
     }
     return url;
 }
 // Add the self-hosted game url as value of gameUrl.
-var gameUrl = 'https://play.gamemonetize.co/682mfby38fh1c41ajtil6os2qt45c9q2/';
+var gameUrl = 'https://html5.gamedistribution.com/823ae62e275a4b82a07a1d9d2272e7e4/?gd_sdk_referrer_url=https://gamedistribution.com/games/2048-cards';
 document
     .getElementById('FunGames')
     .src = gameUrl + '?gd_sdk_referrer_url=' + getParentUrl();
